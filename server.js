f6189623399575bd9ebe09fa5fee5544315dfe61
heart-less-devil <amankk0007@gmail.com>
2025-07-29 04:33:12 +0530

    Remove deprecated MongoDB options

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ console.log('MONGO_URI from env:', process.env.MONGO_URI);
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/gegmo-clone';
 console.log('Using mongoURI:', mongoURI);
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000, // 5 second timeout
   socketTimeoutMS: 45000, // 45 second timeout
 }).then(() => console.log('MongoDB connected')).catch(err => {
